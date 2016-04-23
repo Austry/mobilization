40433db8270a92ce2f484096ecc0c21d30c124b6
Austry <iliazasimov@gmail.com>
2016-04-23 22:32:36 +0300

    Fix gitignore and test

--- app/src/androidTest/java/com/austry/mobilization/ArtistActivityTest.java
+++ app/src/androidTest/java/com/austry/mobilization/ArtistActivityTest.java
@@ -24,7 +24,7 @@ import static android.support.test.espresso.matcher.ViewMatchers.withText;
 import static java.util.Arrays.asList;
 
 @RunWith(AndroidJUnit4.class)
-public class ApplicationTest {
+public class ArtistActivityTest {
 
     @Rule
     public ActivityTestRule<ArtistActivity> mActivityRule = new ActivityTestRule<>(
